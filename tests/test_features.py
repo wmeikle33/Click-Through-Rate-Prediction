dcce732a0235622ea1f0f1ab1ba55011514932bc
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-11-30 22:09:33 +0800

    Update test_features.py

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,1 +1,16 @@
+from ctr_prediction.features import make_pipeline
+import pandas as pd
 
+def test_pipeline_runs_on_small_df():
+    df = pd.DataFrame({
+        "hour": [1, 2, 3],
+        "site_id": ["a", "b", "a"],
+        "click": [0, 1, 0],
+    })
+    X = df[["hour", "site_id"]]
+    y = df["click"]
+
+    pipe = make_pipeline()
+    pipe.fit(X, y)
+    preds = pipe.predict_proba(X)[:, 1]
+    assert preds.shape == (3,)
