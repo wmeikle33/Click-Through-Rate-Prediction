be3028b70c8c060442981022d959cdacf4b06011
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-08 12:49:33 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -7,13 +7,34 @@ from sklearn.metrics import roc_auc_score, accuracy_score, log_loss
 from joblib import dump, load
 from .features import auto_preprocess, split_features_label
 
-def build_pipeline():
+def logistic_pipeline():
     clf = LogisticRegression(max_iter=200)
     pipe = Pipeline(steps=[
         ("prep", None),
         ("clf", clf)
     ])
     return pipe
+
+def decision_tree_pipeline():
+    params = {
+        'task': 'train',
+        'boosting_type': 'gbdt',
+        'objective': 'binary',
+        'metric': { 'binary_logloss'},
+        'num_leaves': 31, # defauly leaves(31) amount for each tree
+        'learning_rate': 0.03,
+        'feature_fraction': 0.7, # will select 70% features before training each tree
+        'bagging_fraction': 0.3, #feature_fraction, but this will random select part of data
+        'bagging_freq': 5, #  perform bagging at every 5 iteration
+        'verbose': 0
+    }
+    
+    gbm = lgb.train(params,
+                    lgb_train,
+                    num_boost_round=5000,
+                    valid_sets=lgb_eval,
+                    early_stopping_rounds=500)
+
 
 def train_eval_save(df, label: str, model_path: str, pipeline_path: str, random_state: int = 42):
     X, y = split_features_label(df, label)
