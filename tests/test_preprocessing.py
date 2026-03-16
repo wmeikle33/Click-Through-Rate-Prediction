d57e10572b9535aee9d9d368af12c2f82a621cbb
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-16 16:25:44 +0800

    Update test_preprocessing.py

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -7,4 +7,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2, 
     random_state=42
 )
+assert len(X_train) == 3
+assert len(X_valid) == 1
+assert X_valid["hour"].iloc[0] == 14102300
 
