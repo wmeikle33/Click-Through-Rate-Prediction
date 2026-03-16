b6f3bbbee22c933e9e5263b54e04fa1a274a0837
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-16 16:04:07 +0800

    Update test_preprocessing.py

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, 
+    test_size=0.2, 
+    random_state=42
+)
 
