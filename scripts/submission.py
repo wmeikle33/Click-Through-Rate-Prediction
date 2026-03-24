23d144467d6cf7cd9e30900c38cb915123a964db
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-25 00:42:16 +0800

    Update submission.py

--- scripts/submission.py
+++ scripts/submission.py
@@ -1,5 +1,5 @@
 my_submission = pd.DataFrame({'id': SampleSubmission['id'], 'click': predictions})
-my_submission.to_csv('submissionnewestversion.csv',index = False)
+my_submission.to_csv('submission.csv',index = False)
 
 def main():
     project_root = Path(__file__).resolve().parents[1]
