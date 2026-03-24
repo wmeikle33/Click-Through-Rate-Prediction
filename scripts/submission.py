55d9f63b83551638a5bb228bf46123e06a0e2c2d
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-25 00:48:40 +0800

    Update submission.py

--- scripts/submission.py
+++ scripts/submission.py
@@ -2,18 +2,7 @@ my_submission = pd.DataFrame({'id': SampleSubmission['id'], 'click': predictions
 my_submission.to_csv('submission.csv',index = False)
 
 def main():
-    project_root = Path(__file__).resolve().parents[1]
-
-    raw_dir = project_root / "data" / "raw"
-    raw_dir.mkdir(parents=True, exist_ok=True)
-
-    download_from_kaggle(raw_dir)
-    unzip_files(raw_dir)
-
-    print("Contents:", list(raw_dir.iterdir()))
-
-    print("Dataset ready in:", raw_dir.resolve())
-
+    !kaggle competitions submit -c titanic -f submission.csv -m "My first API submission"
 
 if __name__ == "__main__":
     main()
