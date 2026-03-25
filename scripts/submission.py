a69622671351aeffffd80d56e7b72553ef2e4093
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-26 00:32:06 +0800

    Update submission.py

--- scripts/submission.py
+++ scripts/submission.py
@@ -1,8 +1,4 @@
-my_submission = pd.DataFrame({'id': SampleSubmission['id'], 'click': predictions})
-my_submission.to_csv('submission.csv',index = False)
-
-def main():
-    !kaggle competitions submit -c titanic -f submission.csv -m "My first API submission"
+from ctr_prediction.submission import main
 
 if __name__ == "__main__":
     main()
